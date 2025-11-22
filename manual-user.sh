0a7b323386b5ae8e00b2fc4d99850c6e2ff64178
anbakely <anbakely42@tntech.edu>
2025-11-21 21:27:56 -0600

    finished manual user interface

--- manual-user.sh
+++ manual-user.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
+source ./automated-user.sh 
 
+manual_user() {
+while true; do
+	echo "Please enter your first name"
+	read user_first_name
+	if [[ "$user_first_name" =~ ^[A-Za-z]+$ ]]; then
+		break
+	else
+		echo "Try again with a string only letters."
+	fi
+done
+while true; do
+	echo "Please enter your last name"
+	read user_last_name
+	if [[ "$user_last_name" =~ ^[A-Za-z]+$ ]]; then
+		break
+	else
+		echo "Try again with a string just letters"
+		fi
+done
+
+generate_username(user_first_name, user_last_name)
+generate_password(user_first_name, user_last_name)
+}
