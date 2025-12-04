8e98449d972f468ff478a28a2ef2cd560d0de8f1
Ricky Newbold <rjnewbold42@tntech.edu>
2025-12-04 13:37:33 -0600

    Added random passwords and credential saving

--- automated-user.sh
+++ automated-user.sh
@@ -24,13 +24,10 @@ generate_username() {
 # Generates password
 # -------------------------
 generate_password() {
-    FIRST="$1"
-    LAST="$2"
-
-    CLEAN_FIRST=$(echo "$FIRST" | tr '[:upper:]' '[:lower:]')
-    CLEAN_LAST=$(echo "$LAST" | tr '[:upper:]' '[:lower:]')
-
-    echo "${CLEAN_FIRST}${CLEAN_LAST}DEELTECH"
+    # use /dev/urandom to generate the random string
+    # each character is a random letter, digit, or special character
+    tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c 12
+    echo
 }
 
 # -------------------------
@@ -51,6 +48,10 @@ main() {
     FIRST_NAME=$1
     LAST_NAME=$2
 
+    BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    DATA_DIR="${BASE_DIR}/data"
+    OUTPUT="${DATA_DIR}/logins.txt"
+
     # Make sure there is a first and last name
     if [[ -z "$FIRST_NAME" || -z "$LAST_NAME" ]]; then
         echo "Error: First and last name required."
@@ -58,7 +59,7 @@ main() {
     fi
 
     USERNAME=$(generate_username "$FIRST_NAME" "$LAST_NAME")
-    PASSWORD=$(generate_password "$FIRST_NAME" "$LAST_NAME")
+    PASSWORD=$(generate_password)
 
     # Check if user exists
     if user_exists "$USERNAME"; then
@@ -70,6 +71,9 @@ main() {
     if sudo useradd -m "$USERNAME"; then
         echo "${USERNAME}:${PASSWORD}" | sudo chpasswd
         echo "Created user '$USERNAME'"
+
+        echo "Username: $USERNAME, Password: $PASSWORD" >> "$OUTPUT"
+
         return 0
     else
         echo "Failed to create user '$USERNAME'"
